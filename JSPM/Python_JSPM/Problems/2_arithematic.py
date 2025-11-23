f40ddcc5e4d184b4cc9e16354051019b35b83e9a
Suyash Padir <suyashpadir01@gmail.com>
2025-11-23 17:20:09 +0530

    Update and rename 2_arithematic_pr.py to 2_arithematic.py

---
+++ JSPM/Python_JSPM/Problems/2_arithematic.py
@@ -0,0 +1,12 @@
+# Arthimetic Operations
+num1 = int(input("Enter 1st Number:"))
+num2 = int(input("Enter 2nd Number:"))
+sum = num1 + num2
+multiplication = num1 * num2
+division = num1/num2
+remainder = num1 % num2
+
+print("The Sum of Given Numbers is:",sum)
+print("The Multiplication of Given Numbers is:",multiplication)
+print("The Division of Given Numbers is:",division)
+print("The Remainder of Division of Given Numbers is:",remainder)
--- JSPM/Python_JSPM/Problems/2_arithematic_pr.py
+++
@@ -1,5 +0,0 @@
-# Arthimetic Operations
-print(5+2)#operands & operator
-print(5/2)
-print(5-2)
-print(5*2)
