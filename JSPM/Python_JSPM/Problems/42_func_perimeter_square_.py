1546ee8cda1d149dfa96378faa2e0f0a047da119
Suyash Padir <suyashpadir01@gmail.com>
2025-11-23 17:33:18 +0530

    Update and rename 42_perimeter_square_pr.py to 42_func_perimeter_square_.py

---
+++ JSPM/Python_JSPM/Problems/42_func_perimeter_square_.py
@@ -0,0 +1,5 @@
+def perimeter_square(side):
+    return 4 * side
+s = float(input("Enter side of square: "))
+perimeter = perimeter_square(s)
+print("Perimeter of Square =", perimeter)
--- JSPM/Python_JSPM/Problems/42_perimeter_square_pr.py
+++
@@ -1,6 +0,0 @@
-side = 9
-def peri_square(side):
-    a = side*4
-    return(a)
-a = peri_square(side)
-print(f"The Perimeter of Square with side {side} is {a}")
