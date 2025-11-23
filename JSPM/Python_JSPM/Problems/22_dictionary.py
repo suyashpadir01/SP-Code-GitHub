5c757679fdd003dbef7678e9471d1d08d10f084e
Suyash Padir <suyashpadir01@gmail.com>
2025-11-23 18:20:32 +0530

    Update and rename 22_dictionary_pr.py to 22_dictionary.py

---
+++ JSPM/Python_JSPM/Problems/22_dictionary.py
@@ -0,0 +1,7 @@
+students = {
+    "student1": {"name": "Suyash", "age": 18},
+    "student2": {"name": "Saksham", "age": 19}
+}
+
+print(students["student1"]["name"])  
+print(students["student2"]["age"])    
