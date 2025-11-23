00b349fc1a5eb2cca2700d41d6df207bf1573963
Suyash Padir <suyashpadir01@gmail.com>
2025-11-23 18:10:37 +0530

    Update and rename 13_greater_amongstpr.py to 13_greater.py

---
+++ JSPM/Python_JSPM/Problems/13_greater.py
@@ -0,0 +1,7 @@
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+#To check which number is greater
+if(a>b):
+    print("Greater Number is:",a)
+else:
+    print("Greater Number is:",b)
--- JSPM/Python_JSPM/Problems/13_greater_amongstpr.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Enter the first number a:"))
-b = int(input("Enter the Second number b:"))
-
-if (a > b):
-    print(str(a) + " is greater than " + str(b))
-    # print( a + " is greater than " + b) #concatination
-else:
-    print(str(b) + " is greater than " + str(a))
