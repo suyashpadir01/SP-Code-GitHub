f53c821a90591c7ba6ce299e1ae4a48d4c1777d0
Suyash Padir <suyashpadir01@gmail.com>
2025-11-23 18:16:13 +0530

    Update and rename 18_dictionary_creation_pr.py to 18_dictionary_creation.py

---
+++ JSPM/Python_JSPM/Problems/18_dictionary_creation.py
@@ -0,0 +1,7 @@
+student = {}
+student["name"] = "Suyash"
+student["age"] = 19
+student["course"] = "Engineering"
+student["college"] = "JSPM RSCOE"
+print("Student Dictionary:")
+print(student)
--- JSPM/Python_JSPM/Problems/18_dictionary_creation_pr.py
+++
@@ -1,4 +0,0 @@
-students = {
-    "student1": {"name": "Soham", "age": 18},
-    "student2": {"name": "Ritesh", "age": 19}
-}
