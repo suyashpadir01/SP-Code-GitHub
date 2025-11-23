c092b4e75ad136d63898134df8e6fa2342bed01f
Suyash Padir <suyashpadir01@gmail.com>
2025-11-23 17:39:12 +0530

    Update and rename 37_func_sub_pr.py to 37_func_sub.py

---
+++ JSPM/Python_JSPM/Problems/37_func_sub.py
@@ -0,0 +1,6 @@
+def substract(a,b):
+    return a-b
+print("Arithmatic Operations using functions")
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+print("Subtraction:", substract(a, b))
