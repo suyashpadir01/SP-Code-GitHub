1c757c423cbe191a7f9aa7719ff669ac769e2552
Suyash Padir <suyashpadir01@gmail.com>
2025-11-23 17:40:18 +0530

    Update and rename 38_func_mul_pr.py to 38_func_multi.py

--- JSPM/Python_JSPM/Problems/38_func_mul_pr.py
+++
@@ -1,7 +0,0 @@
-num1 = 5
-num2 = 9
-def mul_num(num1, num2):
-    a = num1*num2
-    return(a)
-a = mul_num(num1, num2)
-print(a)
---
+++ JSPM/Python_JSPM/Problems/38_func_multi.py
@@ -0,0 +1,6 @@
+def multiply(a,b):
+    return a*b
+print("Arithmatic Operations using functions")
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+print("Multiplication:", multiply(a, b))
