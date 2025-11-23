0331fdc9999ed07fcd2972d4e34e25e6868b7e31
Suyash Padir <suyashpadir01@gmail.com>
2025-11-23 17:42:01 +0530

    Update and rename 3_swap_pr.py to 3_num_swap.py

---
+++ JSPM/Python_JSPM/Problems/3_num_swap.py
@@ -0,0 +1,7 @@
+a = 5
+b= 6
+print(f"Before swawping values of a = {a},b = {b}")
+temp = a
+a = b
+b = temp
+print(f"After swawping values of a = {a},b = {b}")
--- JSPM/Python_JSPM/Problems/3_swap_pr.py
+++
@@ -1,8 +0,0 @@
-# Number Swaping
-a = 5
-b= 6
-print(f"Before swawping values of a = {a},b = {b}")#fstring
-temp = a
-a = b
-b = temp
-print(f"After swawping values of a = {a},b = {b}")
