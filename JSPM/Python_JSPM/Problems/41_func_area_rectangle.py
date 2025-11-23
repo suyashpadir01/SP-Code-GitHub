43e400438348ffba70f15c8ebf6da6e02ffe83b8
Suyash Padir <suyashpadir01@gmail.com>
2025-11-23 17:26:52 +0530

    Update and rename 41_area_rectangle_pr.py to 41_func_area_rectangle.py

---
+++ JSPM/Python_JSPM/Problems/41_func_area_rectangle.py
@@ -0,0 +1,6 @@
+def area_rectangle(length, width):
+    return length * width
+l = float(input("Enter length of rectangle: "))
+w = float(input("Enter width of rectangle: "))
+area = area_rectangle(l, w)
+print("Area of Rectangle =", area)
